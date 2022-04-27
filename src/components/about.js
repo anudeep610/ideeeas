fad0554533ed3bfe85c59be878f7093fccb30b13
AishwaryaArun15 <57674575+AishwaryaArun15@users.noreply.github.com>
2022-04-28 01:48:53 +0530

    Add files via upload

---
+++ src/components/about.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./css/about.css";
+
+
+export default function about() {
+  return (
+    <div className="section-about-box">
+        <div className="about-image-box">
+            <img src="images/team.png" alt="team photo" />
+        </div>
+        <div className="about-text-box">
+            <h1 className="heading-tertiary">Team IEEE</h1>
+            <p className="about-para">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae aliquam, expedita eius eum nobis voluptatum dolorem optio saepe sequi? Mollitia numquam officiis ad unde autem recusandae, quia molestias accusamus molestiae ducimus minus ab nam dicta distinctio, cupiditate quam nihil voluptas dignissimos. Nostrum quod explicabo, enim sint ab doloribus commodi fugit.
+            </p>
+        </div>
+    </div>
+  )
+}
---
+++ src/components/card.js
@@ -0,0 +1,33 @@
+import "./css/card.css";
+import React from "react";
+import Button from "./button"; 
+
+export default function Card(props) {
+  const id= String(props.name);
+  const intro = String(props.intro);
+  const id_back =String(props.name)+"-back";
+  console.log(id_back)
+  return (
+    <div className="card">
+      <div className="front" id={id}>
+        <h4 className="card__heading">
+          {/* <span class="card__heading-span">
+                    {" "}
+                    {props.name}
+                  </span> */}
+        </h4>
+        <div className="card__details">
+          {/* <ul>
+            <li key="1">{props.h1}</li>
+          </ul> */}
+        </div>
+      </div>
+      <div className="back" id={id_back}>
+        <div className="card__cta">
+          <div className="card_desc">{intro}</div>
+          <Button text="Register Now" dest={props.name} num={props.id} className="bookNow"/>
+        </div>
+      </div>
+    </div>
+  );
+}
