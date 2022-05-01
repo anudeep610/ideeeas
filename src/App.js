28c99b71b803b00b2c671e447ff3a6cb81447b42
Anudeep <anudeep.hanuman@gmail.com>
2022-05-01 12:55:05 +0530

    added deleted fies

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import {Route, Routes } from "react-router-dom";
+import Home from "./components/home";
+import EventPage from "./components/EventPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:eventNum" element={<EventPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
